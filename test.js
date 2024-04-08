d9f8d0d5b1c1c09a7a7d9ecb9340fdb86135bbcf
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-04-08 16:23:07 +0000

    jest

--- test.js
+++ test.js
@@ -1,15 +1,19 @@
 test("One euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
-    const { fromEuroToDollar,fromDollarToYen,fromYenToPound } = require('./app.js');
+    const { fromEuroToDollar} = require('./app.js');
 
     // Uso la función como debe ser usada
-    const dollars = fromEuroToDollar(3.5);fromDollarToYen(156.5);fromYenToPound(0.87);
-
+    const dollar = fromEuroToDollar(3.5)
+    const expect = 3.5 * 1.07
     // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07; 1.07 * 156.5; 156.5 * 0.87;
+   
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(dollar).toBe(expect); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+test("One dollar should be 156,5 yen", function() {
+    const {fromDollarToYen} = require('./app.js');
+    
 })
 
 
